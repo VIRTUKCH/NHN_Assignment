b968ed2f165496816fbc3ffe48620602c9a0cbb8
Virtus_Chae <ybd4731@gmail.com>
2024-03-05 11:24:53 +0900

    docs : deleted comment - 주석 제거

--- 3. Java Programming Practice 1주차 과제/rational/src/main/java/com/nhnacademy/Rational.java
+++ 3. Java Programming Practice 1주차 과제/rational/src/main/java/com/nhnacademy/Rational.java
@@ -2,10 +2,6 @@ package com.nhnacademy;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger; // core를 import하면 안 됨.
-// import org.apache.logging.log4j.core.Logger;
-
-// '모든' Integer 범위의 테스트 케이스를 통과하기 위해서는 => Long 멤버 변수가 필수적인 것 같다.
-// 그렇게 따지면 무조건 BigInteger을 해야 하는 거 아닐까?
 
 /**
  * 유리수
@@ -18,8 +14,8 @@ public class Rational {
     // 로그에 이름을 주면 특정한 로그만 가져올 수 있다.
     // getSimpleName을 하면 클래스 이름만 나옴.
     Logger logger = LogManager.getLogger(this.getClass().getName());
-    // 이렇게 하면 static하게 관리할 수 있음.
     // static Logger logger = LogManager.getLogger(Rational.class.getName());
+    // 이렇게 하면 static하게 관리할 수 있음.
 
     public long getNumerator() {
         return this.numerator;

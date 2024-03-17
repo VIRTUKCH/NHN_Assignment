0973b6678f5c90fe5ceab4f4a9eccc2c835fd1cb
Virtus_Chae <ybd4731@gmail.com>
2024-03-17 12:47:22 +0900

    feat : g에서 g2d로 변경

--- 4. Java Programming Practice 2주차 과제/canon_assignment/src/main/java/com/nhnacademy/Canon.java
+++ 4. Java Programming Practice 2주차 과제/canon_assignment/src/main/java/com/nhnacademy/Canon.java
@@ -2,41 +2,43 @@ package com.nhnacademy;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class Canon extends MovableBox {
     public static final Color DEFAULT_COLOR = Color.BLACK;
-    
+
     public Canon(int x, int y, int width, int height, Color color) {
         super(x, y, width, height, color);
     }
 
     @Override
     public void paint(Graphics g) {
-        if (g == null) {
+        Graphics2D g2d = (Graphics2D) g;
+        if (g == null || g2d == null) {
             throw new IllegalArgumentException();
         }
 
         Color originalColor = g.getColor();
 
-        g.setColor(getColor());
+        g2d.setColor(getColor());
 
         // 탱크 아래 사각형
-        g.fillRect(getBounds().getMinX() - 25, getBounds().getMinY(),
+        g2d.fillRect(getBounds().getMinX() - 25, getBounds().getMinY(),
                 getBounds().getWidth() + 50, getBounds().getHeight());
 
         // 탱크 머리 원
-        g.fillOval(getCenterX() - 50, getCenterY() - 100, getWidth(), getHeight());
+        g2d.fillOval(getCenterX() - 50, getCenterY() - 100, getWidth(), getHeight());
 
         // 탱크 포
-        g.fillRect(getCenterX(), getCenterY() - 80, 100, 20);
+        g2d.fillRect(getCenterX(), getCenterY() - 80, 100, 20);
 
-        g.setColor(Color.GRAY);
+        g2d.setColor(Color.GRAY);
 
         // 테두리 그리기
-        g.drawRect(getBounds().getMinX() - 25, getBounds().getMinY(),
+        g2d.drawRect(getBounds().getMinX() - 25, getBounds().getMinY(),
                 getBounds().getWidth() + 50, getBounds().getHeight());
 
-        g.setColor(originalColor);
+        g2d.setColor(originalColor);
     }
 
     @Override

4c087b125a33e2f0c56e0a4a49715a9f1c5591cf
Virtus_Chae <ybd4731@gmail.com>
2024-03-21 15:45:58 +0900

    feat : 최종 완료

--- 5. Network Programming 1주차 과제/simple_nc/src/main/java/com/nhnacademy/Main.java
+++ 5. Network Programming 1주차 과제/simple_nc/src/main/java/com/nhnacademy/Main.java
@@ -22,52 +22,106 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // 1. 옵션 관리
         Options options = new Options();
-        Option serVerOption = new Option("l", true, "ServerOption"); // -l 옵션은 뒤에 값이 이어서 온다.
-        options.addOption(serVerOption);
+        Option serverOption = new Option("l", true, "ServerOption"); // -l 옵션은 뒤에 값이 이어서 온다.
+        options.addOption(serverOption);
 
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine commandLine = parser.parse(options, args);
-            // 1-1) -l 옵션 -> Server로 동작하게 만들기
-            if (commandLine.hasOption(serVerOption.getOpt())) {
-                int portNumberOfServer = Integer.parseInt(commandLine.getOptionValue(serVerOption.getOpt()));
+            // 1-1) -l 옵션 -> Server로 동작하게 만들기 -> 내가 서버를 열었다. ex) -l 1234 == 내가 서버다
+            if (commandLine.hasOption(serverOption.getOpt())) {
+                int portNumberOfServer = Integer.parseInt(commandLine.getOptionValue(serverOption.getOpt()));
+
                 try (ServerSocket serverSocket = new ServerSocket(portNumberOfServer)) {
+                    System.out.println("서버가 " + portNumberOfServer + " 포트에서 대기중입니다.");
                     Socket socket = serverSocket.accept(); // 서버를 열어 둔다.
-                    System.out.println("Connected : " + serverSocket.getInetAddress().getHostAddress());
-                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                    String str;
-                    while (!(str = br.readLine()).equals("exit")) {
-                        bw.write("서버에서 전송한 메시지 : " + str + "\n");
-                        bw.flush();
+                    System.out.println("클라이언트가 연결 되었습니다.");
+
+                    // 메세지를 보내는 쓰레드
+                    Thread sendMessageThread = new Thread(() -> {
+                        try {
+                            BufferedReader serverMessageReader = new BufferedReader(new InputStreamReader(System.in));
+                            BufferedWriter serverMessageWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                            String sendMessage;
+                            while ((sendMessage = serverMessageReader.readLine()) != null) {
+                                serverMessageWriter.write(sendMessage + "\n");
+                                serverMessageWriter.flush();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+                    // 메세지를 받는 쓰레드
+                    Thread receiveMessageThread = new Thread(() -> {
+                        try {
+                            BufferedReader clientMessageReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            String receivedMessage;
+                            while ((receivedMessage = clientMessageReader.readLine()) != null) {
+                                System.out.println("클라이언트: " + receivedMessage);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+                    sendMessageThread.start();
+                    receiveMessageThread.start();
+
+                    try {
+                        sendMessageThread.join();
+                        receiveMessageThread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
             }
 
             // nc -l 1234로 일단 서버를 열어야 할 것.
-            // 1-2) 옵션이 없으면 -> Client로 동작하게 만들기
+            // 1-2) 옵션이 없으면 -> Client로 동작하게 만들기. ex) 그냥 두면 -> 1234로 연결됨 -> 나는 Client다.
             else {
                 String host = "localhost";
                 int portNumberOfClient = 1234;
 
                 try (Socket socket = new Socket(host, portNumberOfClient)) {
                     System.out.println("서버에 연결되었습니다.");
-                    System.out.println("Local address : " + socket.getLocalAddress()); // 소켓의 로컬 주소
-                    System.out.println("Local port : " + socket.getLocalPort()); // 소켓의 로컬 포트
-                    System.out.println("Remote address : " + socket.getRemoteSocketAddress()); // 소켓의 원격 주소
-                    System.out.println("Remote port : " + portNumberOfClient);
+        
+                    // 메시지를 받는 스레드
+                    Thread receiveMessageThread = new Thread(() -> {
+                        try {
+                            BufferedReader serverBufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            String receivedMessage;
+                            while ((receivedMessage = serverBufferedReader.readLine()) != null) {
+                                System.out.println("서버: " + receivedMessage);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        
+                    // 메시지를 보내는 스레드
+                    Thread sendMessageThread = new Thread(() -> {
+                        try {
+                            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                            String str;
+                            while (!(str = br.readLine()).equals("exit")) {
+                                bw.write(str + "\n");
+                                bw.flush();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    receiveMessageThread.start();
+                    sendMessageThread.start();
 
-                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
-                    String str;
-                    while (!(str = br.readLine()).equals("exit")) {
-                        bw.write(str + "\n");
-                        bw.flush();
+                    try {
+                        sendMessageThread.join();
+                        receiveMessageThread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-
-                } catch (Exception e) {
-                    System.err.println("Host를 찾을 수 없습니다. " + e.getMessage());
                 }
             }
         } catch (ParseException e) {

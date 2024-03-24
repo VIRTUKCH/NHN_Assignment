5d95c724f07ed387647e564fff1b40010c7a2697
Virtus_Chae <ybd4731@gmail.com>
2024-03-24 22:33:38 +0900

    복구

---
+++ 5. Network Programming 1주차 과제/multi_communicate/src/main/java/com/nhnacademy/ClientHandler.java
@@ -0,0 +1,44 @@
+package com.nhnacademy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+// 서버 -> 클라이언트 주고 받기
+public class ClientHandler extends Thread {
+    int index;
+    BufferedReader clientMessageReader;
+    BufferedWriter clientMessageWriter;
+    BufferedReader serverMessageReader;
+    BufferedWriter serverMessageWriter;
+
+    public ClientHandler(Socket clientSocket, int index) {
+        this.index = index;
+        try {
+            clientMessageReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            clientMessageWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            serverMessageReader = new BufferedReader(new InputStreamReader(System.in));
+            serverMessageWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        // 클라이언트의 메시지 받기
+        try {
+            String msgOfClient;
+            while ((msgOfClient = clientMessageReader.readLine()) != null) {
+                // 클라이언트로부터 메시지를 받아서 처리하는 로직을 구현
+                serverMessageWriter.write("클라이언트 " + index + "의 메세지: " + msgOfClient + "\n");
+                serverMessageWriter.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

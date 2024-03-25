32ff36217c7712408ecdc5e0519128f60611be2d
Virtus_Chae <ybd4731@gmail.com>
2024-03-25 22:52:09 +0900

    설계 및 목표 작성

--- 5. Network Programming 1주차 과제/multi-chatting/src/main/java/com/nhnacademy/Main.java
+++ 5. Network Programming 1주차 과제/multi-chatting/src/main/java/com/nhnacademy/Main.java
@@ -6,12 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Options options = new Options();
 
+        // p 옵션이 있으면 서버로 보자.
         Option serverOption = Option.builder("p")
                 .argName("portNumber")
                 .hasArg() // 이 옵션은 인자(값)을 가질 수 있음
                 .optionalArg(true) // 옵션의 인자(값)은 선택적임
                 .desc("This is an option that may or may not have a value.")
                 .build();
+        
+        // c 옵션이 있으면 Client로 보자.
         Option clientOption = new Option("c", false, "Run as client");
 
         options.addOption(serverOption);
